85c79372efebfd8de833da43267d57146a1cc357
luna156 <v010309@naver.com>
2024-11-05 14:16:22 +0900

    first assignment

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from decimal import Decimal
 
+from django.views.generic.edit import DeletionMixin
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
@@ -22,6 +23,19 @@ class CalculatorAPIView(GenericAPIView):
 
         result: Decimal = None
         ## assignment1: 이곳에 과제를 작성해주세요
+        input_a = serializer.validated_data['input_a']
+        input_b = serializer.validated_data['input_b']
+        operator = serializer.validated_data['operator']
+
+        if operator == '+':
+            result = Decimal(input_a) + Decimal(input_b)
+        elif operator == '-':
+            result = Decimal(input_a) - Decimal(input_b)
+        elif operator == '*':
+            result = Decimal(input_a) * Decimal(input_b)
+        elif operator == '/':
+            result = Decimal(input_a) / Decimal(input_b)
+
         ## end assignment1
 
         # serialization
