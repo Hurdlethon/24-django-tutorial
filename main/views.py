48df82e703c32c6337d0eaaa53aa53441c402b0b
Seonung Kim <su8101@naver.com>
2024-11-06 22:09:08 +0900

    first assignment

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from decimal import Decimal
 
+from django.core.serializers import serialize
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
@@ -22,7 +23,24 @@ class CalculatorAPIView(GenericAPIView):
 
         result: Decimal = None
         ## assignment1: 이곳에 과제를 작성해주세요
+
+        input_a=Decimal(serializer.validated_data['input_a'])
+        input_b=Decimal(serializer.validated_data['input_b'])
+        how=serializer.validated_data['operator']
+
+        if how=='+':
+            result=input_a + input_b
+        elif how=='-':
+            result = input_a - input_b
+        elif how=='*':
+            result = input_a * input_b
+        elif how=='/':
+            result = input_a / input_b
+
+
         ## end assignment1
 
         # serialization
         return Response(CalculatorResponseSerializer({"result": result}).data)
+
+
