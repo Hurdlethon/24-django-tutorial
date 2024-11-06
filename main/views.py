c051368ab24b8e053e2f45868183115fd1df9143
EauLune01 <161578205+EauLune01@users.noreply.github.com>
2024-11-06 11:59:17 +0900

    Assignment 2 complete

--- main/views.py
+++ main/views.py
@@ -17,6 +17,14 @@ class StudentListAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
     """
 
     ### assignment2: 이곳에 과제를 작성해주세요
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
     ### end assignment2
 
 
@@ -30,4 +38,15 @@ class StudentAPIView(
     """
 
     ### assignment2: 이곳에 과제를 작성해주세요
-    ### end assignment2
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+        ### end assignment2
