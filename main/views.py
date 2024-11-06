5a167d9f61c011d3724d4932ea91d0cb943c6d86
EauLune01 <161578205+EauLune01@users.noreply.github.com>
2024-11-06 16:18:23 +0900

    re

--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+)
+from rest_framework.generics import GenericAPIView
 from .models import Student
 from .serializers import StudentSerializer
 
-class StudentListAPIView(ListCreateAPIView):
+class StudentListAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
     """
     GET: 학생 목록 조회
     POST: 학생 추가
@@ -12,7 +19,14 @@ class StudentListAPIView(ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-class StudentAPIView(RetrieveUpdateDestroyAPIView):
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class StudentAPIView(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
     """
     GET: 학생 조회
     PATCH: 학생 수정
@@ -20,3 +34,13 @@ class StudentAPIView(RetrieveUpdateDestroyAPIView):
     """
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
