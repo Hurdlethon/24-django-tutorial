0afd971b988f9ef99b87dac7b826eb6ff2d2df61
EauLune01 <161578205+EauLune01@users.noreply.github.com>
2024-11-06 16:15:22 +0900

    re

--- main/serializers.py
+++ main/serializers.py
@@ -1,15 +1,11 @@
 from rest_framework import serializers
-from .models import Student
-
+from .models import Student  # Student 모델 임포트
 
 class StudentSerializer(serializers.ModelSerializer):
     """
     학생 모델에 대한 Serializer
     """
 
-    ### assignment2: 이곳에 과제를 작성해주세요
     class Meta:
-        model = Student  # Student 모델 지정
-        fields = ['id', 'name', 'student_number', 'primary_major']
-
-    ### end assignment2
+        model = Student  # 모델 정의
+        fields = ['id', 'name', 'student_number', 'primary_major']  # 필요한 필드 설정
--- main/views.py
+++ main/views.py
@@ -1,53 +1,22 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.mixins import (
-    ListModelMixin,
-    CreateModelMixin,
-    RetrieveModelMixin,
-    UpdateModelMixin,
-    DestroyModelMixin,
-)
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework.generics import GenericAPIView
 
-class StudentListAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+class StudentListAPIView(ListCreateAPIView):
     """
     GET: 학생 목록 조회
     POST: 학생 추가
     """
-
-    ### assignment2: 이곳에 과제를 작성해주세요
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    ### end assignment2
-
-
-class StudentAPIView(
-    RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView
-):
+class StudentAPIView(RetrieveUpdateDestroyAPIView):
     """
     GET: 학생 조회
     PATCH: 학생 수정
     DELETE: 학생 삭제
     """
-
-    ### assignment2: 이곳에 과제를 작성해주세요
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-        ### end assignment2
