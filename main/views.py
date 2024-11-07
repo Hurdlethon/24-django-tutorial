63b40e59db1a7c18dc30cd5d5642c400c4509f75
Mr.back <bjh59460@gmail.com>
2024-11-07 12:33:42 +0900

    complete assignment2

--- main/views.py
+++ main/views.py
@@ -42,7 +42,7 @@ class StudentAPIView(
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
     def patch(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+        return self.partial_update(request, *args, **kwargs)
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
     ### end assignment2
