0ac12e1f6085584f9e5186e17a57e72aa5622776
EauLune01 <161578205+EauLune01@users.noreply.github.com>
2024-11-08 11:35:05 +0900

    initial

--- main/views.py
+++ main/views.py
@@ -22,6 +22,23 @@ class CalculatorAPIView(GenericAPIView):
 
         result: Decimal = None
         ## assignment1: 이곳에 과제를 작성해주세요
+        input_a = Decimal(serializer.validated_data.get("input_a"))
+        input_b = Decimal(serializer.validated_data.get("input_b"))
+        operator = serializer.validated_data.get("operator")
+
+        if operator == "+":
+            result = input_a + input_b
+        elif operator == "-":
+            result = input_a - input_b
+        elif operator == "*":
+            result = input_a * input_b
+        elif operator == "/":
+            if not input_b:
+                return Response({"error": "0으로 나눗셈 불가"})
+            else:
+                result = input_a / input_b
+        else:
+            return Response({"error": "연산자에 오류가 있습니다."})
         ## end assignment1
 
         # serialization
