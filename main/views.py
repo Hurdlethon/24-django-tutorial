4091d125065dc7855c38bdd34cc5da87eeb90724
chominser015 <cms6814115@gmail.com>
2024-11-08 23:42:53 +0900

    1

--- main/views.py
+++ main/views.py
@@ -22,6 +22,18 @@ class CalculatorAPIView(GenericAPIView):
 
         result: Decimal = None
         ## assignment1: 이곳에 과제를 작성해주세요
+        input_a = Decimal(serializer.validated_data["input_a"])
+        input_b = Decimal(serializer.validated_data["input_b"])
+        operator = serializer.validated_data["operator"]
+        if(operator=='+'):
+            result=input_a+input_b
+        if (operator == '*'):
+            result = input_a * input_b
+        if (operator == '/'):
+            result = input_a / input_b
+        if (operator == '-'):
+            result = input_a - input_b
+        result=str(result)
         ## end assignment1
 
         # serialization
