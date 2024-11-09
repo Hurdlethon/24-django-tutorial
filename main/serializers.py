3202a388bc1d7295996ff12ee683d201934cbb86
kyumin <kyumin2805@gmail.com>
2024-11-09 23:12:50 +0900

    hw

--- main/serializers.py
+++ main/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
 
+from main.models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
-    """
-    학생 모델에 대한 Serializer
-    """
-
-    ### assignment2: 이곳에 과제를 작성해주세요
-    ### end assignment2
+    class Meta:
+        model = Student
+        fields = '__all__'
--- main/views.py
+++ main/views.py
@@ -8,26 +8,34 @@ from rest_framework.mixins import (
     UpdateModelMixin,
     DestroyModelMixin,
 )
+from main.models import Student
+from main.serializers import StudentSerializer
 
 
 class StudentListAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
-    """
-    GET: 학생 목록 조회
-    POST: 학생 추가
-    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    ### assignment2: 이곳에 과제를 작성해주세요
-    ### end assignment2
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 class StudentAPIView(
     RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView
 ):
-    """
-    GET: 학생 조회
-    PATCH: 학생 수정
-    DELETE: 학생 삭제
-    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    ### assignment2: 이곳에 과제를 작성해주세요
-    ### end assignment2
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
