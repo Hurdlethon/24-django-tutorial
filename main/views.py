d58d3ed07a3aed2b4d41d3e5ca08e94a0eaf9d40
Seonung Kim <su8101@naver.com>
2024-11-10 23:04:27 +0900

    reviseassignment2

--- main/views.py
+++ main/views.py
@@ -5,7 +5,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import login, authenticate
 from main.models import Study, StudyParticipation
-from main.serializers import StudySerializer, LoginSerializer, UserSerializer, StudyParticipationSerializer
+from main.serializers import (
+    StudySerializer,
+    LoginSerializer,
+    UserSerializer,
+    StudyParticipationSerializer
+)
 from rest_framework import generics
 
 class LoginView(GenericAPIView):
@@ -16,18 +21,14 @@ class LoginView(GenericAPIView):
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
         user = authenticate(
             request,
             username=serializer.validated_data["username"],
             password=serializer.validated_data["password"],
         )
-
         if not user:
             raise AuthenticationFailed("아이디 또는 비밀번호가 틀렸습니다")
-
         login(request, user)
-
         return Response()
 
 class SignupView(generics.CreateAPIView):
@@ -55,29 +56,29 @@ class StudyDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 class StudyParticipationListView(ListModelMixin, CreateModelMixin, GenericAPIView):
     """
-    GET: 내 스터디 참여 목록. 남의 것이 조회되면 안됩니다.
-    POST: 내 스터디 참여 목록 추가. 남의 것을 추가할 수 없습니다(HTTP 403 에러)
+    GET: 현재 사용자의 스터디 참여 목록만 조회 가능
+    POST: 현재 사용자의 스터디 참여만 추가 가능 (다른 사용자의 경우 403 에러)
     """
     permission_classes = [IsAuthenticated]
     serializer_class = StudyParticipationSerializer
 
     def get_queryset(self):
-        # 현재 로그인된 사용자의 참여 목록만 조회
+        # 현재 로그인된 사용자의 스터디 참여 목록만 필터링
         return StudyParticipation.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        # 요청자가 자신의 스터디만 추가하도록 제한
+        # 현재 사용자가 아닌 다른 사용자의 참여를 추가하려고 할 경우 예외 발생
         if serializer.validated_data.get('user') != self.request.user:
             raise PermissionDenied("다른 사용자의 참여를 추가할 권한이 없습니다.")
         serializer.save(user=self.request.user)
 
 class StudyParticipationView(DestroyModelMixin, GenericAPIView):
     """
-    DELETE: 내 스터디 참여 목록 제거. 남의 것을 제거할 수 없습니다(HTTP 404 에러)
+    DELETE: 현재 사용자의 스터디 참여 목록만 삭제 가능. 다른 사용자의 경우 404 에러
     """
     permission_classes = [IsAuthenticated]
     serializer_class = StudyParticipationSerializer
 
     def get_queryset(self):
-        # 요청 사용자의 참여 이력만 삭제 가능
+        # 현재 로그인된 사용자의 스터디 참여 이력만 조회
         return StudyParticipation.objects.filter(user=self.request.user)
