3d09d5b6196267f652db1186cf0dcc72bb33ad89
Seonung Kim <su8101@naver.com>
2024-11-10 22:35:20 +0900

    assign3

--- main/serializers.py
+++ main/serializers.py
@@ -1,13 +1,10 @@
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
-
-from main.models import Study, User
-
+from main.models import Study, User, StudyParticipation
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
-
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -20,20 +17,16 @@ class UserSerializer(serializers.ModelSerializer):
         hashed_password = make_password(self.validated_data["password"])
         return super().save(password=hashed_password, **kwargs)
 
-
 class StudyParticipationSerializer(serializers.ModelSerializer):
     """
     StudyParticipation에 해당하는 Serializer
     """
-
-    ### assignment3: 이곳에 과제를 작성해주세요
-    ### end assignment3
-
+    class Meta:
+        model = StudyParticipation
+        fields = ["id", "study", "user"]
 
 class StudySerializer(serializers.ModelSerializer):
-    created_by = serializers.PrimaryKeyRelatedField(
-        read_only=True,
-    )
+    created_by = serializers.PrimaryKeyRelatedField(read_only=True)
     studyparticipation_set = StudyParticipationSerializer(many=True, read_only=True)
 
     class Meta:
--- main/views.py
+++ main/views.py
@@ -1,23 +1,12 @@
-# Create your views here.
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed, PermissionDenied
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import (
-    ListModelMixin,
-    DestroyModelMixin,
-    CreateModelMixin,
-)
+from rest_framework.mixins import ListModelMixin, DestroyModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
-from main.models import Study
 from django.contrib.auth import login, authenticate
-from main.serializers import (
-    StudySerializer,
-    LoginSerializer,
-    UserSerializer,
-)
+from main.models import Study, StudyParticipation
+from main.serializers import StudySerializer, LoginSerializer, UserSerializer, StudyParticipationSerializer
 from rest_framework import generics
-
 
 class LoginView(GenericAPIView):
     authentication_classes = []
@@ -27,71 +16,57 @@ class LoginView(GenericAPIView):
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
         user = authenticate(
             request,
             username=serializer.validated_data["username"],
             password=serializer.validated_data["password"],
         )
-
         if not user:
             raise AuthenticationFailed("아이디 또는 비밀번호가 틀렸습니다")
-
         login(request, user)
-
         return Response()
-
 
 class SignupView(generics.CreateAPIView):
     authentication_classes = []
     permission_classes = []
     serializer_class = UserSerializer
 
-
 class StudyListView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
-
     queryset = Study.objects.all()
     serializer_class = StudySerializer
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
-
 class StudyDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
-
     queryset = Study.objects.all()
     serializer_class = StudySerializer
 
     def get_queryset(self):
         if self.request.method == "GET":
             return super().get_queryset()
-
         return super().get_queryset().filter(created_by=self.request.user)
 
+class StudyParticipationListView(ListModelMixin, CreateModelMixin, GenericAPIView):
 
-class StudyParticipationListView(
-    ListModelMixin,
-    CreateModelMixin,
-    GenericAPIView,
-):
-    """
-    GET: 내 스터디 참여 목록. 남의 것이 조회되면 안됩니다.
-    POST: 내 스터디 참여 목록 추가. 남의 것을 추가할 수 없습니다(HTTP 403 에러)
-    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = StudyParticipationSerializer
 
-    ### assignment3: 이곳에 과제를 작성해주세요
-    ### end assignment3
+    def get_queryset(self):
+        return StudyParticipation.objects.filter(user=self.request.user)
 
+    def perform_create(self, serializer):
+        user_data = serializer.validated_data.get('user')
+        if user_data != self.request.user:
+            raise PermissionDenied("다른 사용자의 참여를 추가할 권한이 없습니다.")
+        serializer.save(user=self.request.user)
 
-class StudyParticipationView(
-    DestroyModelMixin,
-    GenericAPIView,
-):
-    """
-    DELETE: 내 스터디 참여 목록 제거. 남의 것을 제거할 수 없습니다(HTTP 404 에러)
-    """
+class StudyParticipationView(DestroyModelMixin, GenericAPIView):
 
-    ### assignment3: 이곳에 과제를 작성해주세요
-    ### end assignment3
+    permission_classes = [IsAuthenticated]
+    serializer_class = StudyParticipationSerializer
+
+    def get_queryset(self):
+        return StudyParticipation.objects.filter(user=self.request.user)
